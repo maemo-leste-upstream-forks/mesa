438a677f3913a0fe02817327f451012c75a8b10e
Imagination Technologies <powervr@imgtec.com>
2017-11-23 15:50:21 +0000

    dri: use a supported API in driCreateNewContext
    Don't assume the screen supports OpenGL when creating a new context,
    use an API that the screen supports.

--- src/gallium/frontends/dri/dri_util.c
+++ src/gallium/frontends/dri/dri_util.c
@@ -48,6 +48,7 @@
 #include "main/version.h"
 #include "main/debug_output.h"
 #include "main/errors.h"
+#include "util/bitscan.h"
 
 driOptionDescription __dri2ConfigOptions[] = {
       DRI_CONF_SECTION_DEBUG
@@ -489,7 +490,11 @@ driCreateContextAttribs(__DRIscreen *screen, int api,
         mesa_api = API_OPENGLES;
         break;
     case __DRI_API_GLES2:
+        ctx_config.major_version = 2;
+        mesa_api = API_OPENGLES2;
+        break;
     case __DRI_API_GLES3:
+        ctx_config.major_version = 3;
         mesa_api = API_OPENGLES2;
         break;
     case __DRI_API_OPENGL_CORE:
@@ -675,7 +680,14 @@ static __DRIcontext *
 driCreateNewContext(__DRIscreen *screen, const __DRIconfig *config,
                     __DRIcontext *shared, void *data)
 {
-    return driCreateNewContextForAPI(screen, __DRI_API_OPENGL,
+    int apifs;
+
+    apifs = ffs(screen->api_mask);
+
+    if (!apifs)
+        return NULL;
+
+    return driCreateNewContextForAPI(screen, apifs - 1,
                                      config, shared, data);
 }
 
