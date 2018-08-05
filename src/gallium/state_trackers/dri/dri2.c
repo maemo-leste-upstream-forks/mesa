6d8e1532f62db3829f4afb0aa7f52d6b32be21ef
Qiang Yu <yuq825@gmail.com>
2018-08-05 10:36:14 +0800

    st/dri2: use dynamic alloc extensions
    When optional extension increases, use static extension
    is not good. This is for the following adding another
    optional extension.
    
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/state_trackers/dri/dri2.c
+++ src/gallium/state_trackers/dri/dri2.c
@@ -2086,20 +2086,6 @@ static const __DRIextension *dri_screen_extensions[] = {
    NULL
 };
 
-static const __DRIextension *dri_robust_screen_extensions[] = {
-   &driTexBufferExtension.base,
-   &dri2FlushExtension.base,
-   &dri2ImageExtension.base,
-   &dri2RendererQueryExtension.base,
-   &dri2GalliumConfigQueryExtension.base,
-   &dri2ThrottleExtension.base,
-   &dri2FenceExtension.base,
-   &dri2InteropExtension.base,
-   &dri2Robustness.base,
-   &dri2NoErrorExtension.base,
-   NULL
-};
-
 /**
  * This is the driver specific part of the createNewScreen entry point.
  *
@@ -2113,10 +2099,19 @@ dri2_init_screen(__DRIscreen * sPriv)
    struct pipe_screen *pscreen = NULL;
    const struct drm_conf_ret *throttle_ret;
    const struct drm_conf_ret *dmabuf_ret;
+   const int num_optional_extensions = 1;
+   const int num_extensions =
+      ARRAY_SIZE(dri_screen_extensions) + num_optional_extensions;
+   const __DRIextension **extensions, **optional_extensions;
 
-   screen = CALLOC_STRUCT(dri_screen);
+   screen = CALLOC(1, sizeof(*screen) + sizeof(*extensions) * num_extensions);
    if (!screen)
       return NULL;
+
+   extensions = (const __DRIextension **)(screen + 1);
+   memcpy(extensions, dri_screen_extensions, sizeof(dri_screen_extensions));
+   optional_extensions = extensions + ARRAY_SIZE(dri_screen_extensions) - 1;
+   sPriv->extensions = extensions;
 
    screen->sPriv = sPriv;
    screen->fd = sPriv->fd;
@@ -2162,11 +2157,10 @@ dri2_init_screen(__DRIscreen * sPriv)
    }
 
    if (pscreen->get_param(pscreen, PIPE_CAP_DEVICE_RESET_STATUS_QUERY)) {
-      sPriv->extensions = dri_robust_screen_extensions;
+      *optional_extensions = &dri2Robustness.base;
       screen->has_reset_status_query = true;
+      optional_extensions++;
    }
-   else
-      sPriv->extensions = dri_screen_extensions;
 
    configs = dri_init_screen_helper(screen, pscreen);
    if (!configs)
