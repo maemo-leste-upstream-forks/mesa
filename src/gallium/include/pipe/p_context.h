5519406ce607923dadd6517a08116dc9019185a6
Qiang Yu <yuq825@gmail.com>
2018-08-05 20:17:48 +0800

    st/dri2: add optional dri2DamageExtension
    Enable dri2DamageExtension according to
    PIPE_CAP_SET_DAMAGE.
    
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/include/pipe/p_context.h
+++ src/gallium/include/pipe/p_context.h
@@ -927,6 +927,18 @@ struct pipe_context {
     */
    void (*callback)(struct pipe_context *ctx, void (*fn)(void *), void *data,
                     bool asap);
+
+   /**
+    * Set draw damage region
+    *
+    * For implementing EGL_KHR_partial_update. This is a hint for
+    * driver to skip un-damaged region re-draw as an optimization.
+    *
+    * \param ctx        pipe context
+    * \param num_rects  number of rectangles
+    * \patam rects      rectangle array
+    */
+   boolean (*set_damage_region)(struct pipe_context *ctx, unsigned num_rects, int *rects);
 };
 
 
