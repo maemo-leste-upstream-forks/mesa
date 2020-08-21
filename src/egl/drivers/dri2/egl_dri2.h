95b0b92940ab7ae09dd2e33b829224fffe3c4f80
Imagination Technologies <powervr@imgtec.com>
2020-08-21 12:13:28 +0100

    gbm: add pbuffer support
    The EGL backend GLX provider for XWayland may get the EGL configs it
    uses to generate the GLX ones from GBM. That platform doesn't support
    pbuffers. When the client tries to match GLX configs with the DRI ones,
    a mismatch in the pbuffer attributes will result in the GLX config
    being rejected.
    
    Although support for creating pbuffers has been added, this isn't
    required when using the EGL backend GLX provider, as indirect GLX
    isn't supported.

--- src/egl/drivers/dri2/egl_dri2.h
+++ src/egl/drivers/dri2/egl_dri2.h
@@ -473,6 +473,9 @@ struct dri2_egl_surface
    /* surfaceless and device */
    __DRIimage *front;
    unsigned int visual;
+#ifdef HAVE_DRM_PLATFORM
+   struct gbm_bo *front_bo;
+#endif
 
 #ifdef HAVE_WAYLAND_PLATFORM
    void                 *swrast_front;
