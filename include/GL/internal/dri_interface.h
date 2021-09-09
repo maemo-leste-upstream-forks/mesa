c9bfbb4fdd40a7f2cc44ddd65006bd97d3da13a9
Imagination Technologies <powervr@imgtec.com>
2021-09-09 17:55:13 +0100

    egl/wayland: add EGL_BUFFER_PRESERVED support
    When the next back buffer is obtained, and the swap method is
    EGL_BUFFER_PRESERVED, the current buffer contents are preserved with
    a blit, unless the DRI driver is using the image loader extension,
    and the DRI driver requests the current buffer contents
    (__DRI_IMAGE_BUFFER_PREV) as well as the back buffer. This allows the
    blit to be avoided for GPUs that can support EGL_BUFFER_PRESERVED
    directly.
    
    Querying the age of a back buffer may force a blit, but outside of EGL
    conformance testing, this would be an odd thing to do, as the buffer
    age can always be assumed to be 1 with EGL_BUFFER_PRESERVED.
    
    Any received dmabuf feedback may also force a blit.
    
    The IMG PVR driver does not support the DRI2 loader extension, which
    is why the buffer preserved support for the extension is less optimal,
    with the current buffer always being preserved with a blit.
    
    EGL_BUFFER_PRESERVED support has not been added for the Mesa software
    rasteriser.

--- include/GL/internal/dri_interface.h
+++ include/GL/internal/dri_interface.h
@@ -2058,12 +2058,16 @@ enum __DRIimageBufferMask {
     * OpenGL ES API and little change to the SurfaceFlinger API.
     */
    __DRI_IMAGE_BUFFER_SHARED = (1 << 2),
+#define DRI_IMAGE_HAS_BUFFER_PREV
+   __DRI_IMAGE_BUFFER_PREV = (1 << 31),
+
 };
 
 struct __DRIimageList {
    uint32_t image_mask;
    __DRIimage *back;
    __DRIimage *front;
+   __DRIimage *prev;
 };
 
 #define __DRI_IMAGE_LOADER "DRI_IMAGE_LOADER"
